0d2e3c674cfeccd65d2e4bf640c0d96169ef0c28
seibtoy <seibtoy@gmail.com>
2025-06-16 22:15:49 +0300

    initial commit

---
+++ src/app/(pages)/(homepage)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/app/components/ui-kit/button';
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <Button variant={'blackBtn'}>see all</Button>
+    </div>
+  );
+}
